66aa64308d49828f451ae3c1bbbbbe0cbd06afe1
Nuno Carvalho <nuno.miguel99@hotmail.com>
2024-09-25 16:03:15 +0100

    ThermalControl CMake App

---
+++ ThermalControlApp/ThermalControlApp.h
@@ -0,0 +1,25 @@
+﻿// ThermalControlApp.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+#include <unistd.h>  // Para pipe(), fork(), read(), write()
+#include <cstring>   // Para strlen()
+#include <cstdlib>   // Para rand(), srand()
+#include <ctime>     // Para time()
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int fd[2]; // Descritores da pipe
+void createPipe(); // Método para criar a pipe
+void readFromPipe(); // Método para ler da pipe
+void writeToPipe(const char* message); // Método para escrever na pipe
+void simulateTemperature(); // Método para simular a temperatura
+float currentTemperature; // Variável para armazenar a temperatura atual
+
+// TODO: Reference additional headers your program requires here.
